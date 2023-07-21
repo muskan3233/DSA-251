8baf9b4f7102093e9c706270f4dc82495f33ac45
Rehmat <95978634+muskan3233@users.noreply.github.com>
2023-07-21 22:56:16 +0530

    Create 5. next smallest palindrome.java
    21th july 2023

---
+++ Strings/5. next smallest palindrome.java
@@ -0,0 +1,1 @@
+// https://www.codingninjas.com/studio/problems/given-a-string-find-the-next-smallest-palindrome_874577?utm_source=youtube&utm_medium=affiliate&utm_campaign=parikh_youtube&leftPanelTab=0
